38c84d850e943ffe17e7bca94517b5a03a24cd78
Mohamed <89275208+CodeArchix@users.noreply.github.com>
2021-09-21 11:35:50 +0000

    initial commit

---
+++ app/src/main/java/com/codearchix/android/devbyteviewer/work/RefreshDataWork.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.codearchix.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.codearchix.android.devbyteviewer.database.VideosDatabase
+import com.codearchix.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+
+class RefreshDataWork(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    companion object {
+        val WORK_NAME by lazy { "RefreshDataWorker" }
+    }
+
+    override suspend fun doWork(): Result {
+        val database = VideosDatabase.getInstance(applicationContext)
+        val repository = VideosRepository(database)
+
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (exception: HttpException) {
+            Result.retry()
+        }
+    }
+}
+
